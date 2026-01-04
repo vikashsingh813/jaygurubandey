b61b070c51fe746dd023ee7786f8d16e02e22618
Vikash Singh <vikashsingh813@gmail.com>
2026-01-04 17:09:18 +0530

    Inital Commit

--- src/main.jsx
+++ src/main.jsx
@@ -40,6 +40,7 @@ import QuickStart from './pages/QuickStart.jsx'
 import LiveNow from './pages/LiveNow.jsx'
 import AshramVision from './pages/AshramVision.jsx'
 import LatestAmritvani from './pages/LatestAmritvani.jsx'
+import DailyTeachings from './pages/DailyTeachings.jsx'
 
 const router = createBrowserRouter([
   {
@@ -82,6 +83,7 @@ const router = createBrowserRouter([
       { path: 'live-now', element: <LiveNow /> },
       { path: 'ashram-vision', element: <AshramVision /> },
       { path: 'latest-amritvani', element: <LatestAmritvani /> },
+      { path: "daily-teachings", element: <DailyTeachings /> },
     ],
   },
   { path: '*', element: <NotFound /> },
